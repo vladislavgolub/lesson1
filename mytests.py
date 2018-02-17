9a15066671b1618f7db52f63f763c2b5e1ce1689
Vladislav Golub <vladislav.golub@mail.ru>
2018-02-17 16:49:19 +0300

    first commit

---
+++ mytests.py
@@ -0,0 +1,25 @@
+#for a in range(7):
+#	if a==4:
+#		print(a)
+
+
+
+
+
+
+
+
+
+#def summarize(a,b):
+#    return a + b
+
+#sum = summarize(int('18'),2)
+#print(sum)
+
+def not_str (str):
+    str = str.split(' ')
+    for a in range(3):
+        print(str[a]+"\n")
+
+not_str('П р и в е т !')
+
